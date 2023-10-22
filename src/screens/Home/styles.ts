8fc37fc834f9a1a4441896834bfbd84002362c5e
charlenefialho <fialhocharlene>
2023-10-22 13:45:20 -0300

    refactor: height view

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,48 +1,47 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     backgroundColor: '#131016',
-    padding: 24,
-    marginBottom: 10
+    padding: 24
   },
-  eventName:{
-    color: '#FDFCFE',
+  eventName: {
+    color: '#FFF',
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 48
   },
-  eventDate:{
+  eventDate: {
     color: '#6B6B6B',
     fontSize: 16
   },
-  input:{
-    flex:1,
-    height:56,
-    backgroundColor: '#1F1E26',
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: '#1F1E25',
     borderRadius: 5,
-    color: '#FFF',
-    padding:16,
+    color: '#FFFF',
+    padding: 16,
     fontSize: 16,
-    marginRight: 12,
+    marginRight: 12
   },
-  button:{
-    width:56,
-    height:56,
-    borderRadius:5,
-    backgroundColor:'#31CF67',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  buttonText:{
+  buttonText: {
     color: '#FFF',
     fontSize: 24
   },
-  form:{
-    width:'100%',
-    flexDirection:'row',
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
     marginTop: 36,
-    marginBottom: 42,
+    marginBottom: 42
   }
 })
